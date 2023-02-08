3d9d860ea4145c50e98d0803c15f98205d227bfb
Can Toraman <toraman@usc.edu>
2023-02-08 13:35:58 -0800

    geospatial query maxDistance and limit

--- src/db/connect.js
+++ src/db/connect.js
@@ -9,25 +9,27 @@ const client = new MongoClient(uri);
 const dbName = "tweetlogs";
 const collectionName = "tweets";
 
-async function getNearbyLocations(coordinates, limit = 3) {
+async function getNearbyLocations(coordinates) {
   const collection = client.db(dbName).collection(collectionName);
   const cursor = collection.aggregate([
     {
       $geoNear: {
         near: { type: "Point", coordinates },
         distanceField: "dist.calculated",
-        maxDistance: 500000,
+        maxDistance: 100000,
         includeLocs: "dist.location",
         spherical: true,
       },
     },
+    { $sort: { distance: 1 } },
     {
-      $limit: limit,
+      $limit: 5,
     },
   ]);
 
   const set = new Set();
   for await (const doc of cursor) {
+    const proximity = "";
     set.add(
       `${doc.formatted_address}\nhttps://www.google.com/maps/?q=${doc.location.coordinates[0]},${doc.location.coordinates[1]}`
     );
