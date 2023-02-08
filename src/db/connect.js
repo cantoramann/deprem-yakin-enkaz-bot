0a6bbaee57843480bb94d69562da91159dfd267b
Can Toraman <toraman@usc.edu>
2023-02-08 15:12:44 -0800

    search updates

--- src/db/connect.js
+++ src/db/connect.js
@@ -12,40 +12,39 @@ const collectionName = "tweets";
 async function getNearbyLocations(coordinates) {
   try {
     const collection = client.db(dbName).collection(collectionName);
-    const cursor = collection.aggregate([
-      {
-        $geoNear: {
-          near: { type: "Point", coordinates },
-          distanceField: "dist.calculated",
-          maxDistance: 100000,
-          includeLocs: "dist.location",
-          spherical: true,
+    const data = await collection
+      .aggregate([
+        {
+          $geoNear: {
+            near: { type: "Point", coordinates },
+            distanceField: "dist.calculated",
+            maxDistance: 100000,
+            includeLocs: "dist.location",
+            spherical: true,
+          },
         },
-      },
-      {
-        $sort: {
-          "dist.calculated": 1,
+        {
+          $limit: 5,
         },
-      },
-      {
-        $limit: 5,
-      },
-    ]);
+      ])
+      .toArray();
 
     const set = new Set();
     let c = 1;
-    for await (const doc of cursor) {
-      const proximity = "";
-      set.add(
-        `${c}.\Mesaj: ${doc.raw.full_text}\n\nLink: https://www.google.com/maps/?q=${doc.location.coordinates[0]},${doc.location.coordinates[1]}`
-      );
-      c++;
-    }
+    data.forEach((doc) => {
+      {
+        const proximity = "";
+        set.add(
+          `${c}.\Mesaj: ${doc.raw.full_text}\n\nLink: https://www.google.com/maps/?q=${doc.location.coordinates[0]},${doc.location.coordinates[1]}`
+        );
+        c++;
+      }
+    });
     return set.size
       ? Array.from(set).join("\n\n")
       : "Yakınınızda Twitter üzerinden enkaz bulunamadı.";
   } catch (e) {
-    console.log(e);
+    console.log(e.message);
     return "Bir hata oluştu.";
   }
 }
